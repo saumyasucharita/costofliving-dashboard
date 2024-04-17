a04df8dd96c84265e60d6007f09312513a9d1170
saumya <saumya.sucharita@gmail.com>
2024-04-16 21:03:31 -0700

    Fetch data from json file in react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
 function App() {
+console.log('Starting front-end project');
+const [results, setResults] = useState([]);
+
+function fetchCityJSONData() {
+ console.log('In fetch function');	
+    return fetch("./cost_of_living_data.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+           console.log(data);
+           return data;})
+        .catch((error) => {
+            console.error("Error fetching or parsing data:", error);
+            throw error; 
+        }); // Get the first 10 objects from the array
+}
+useEffect(() => {
+        console.log('Page rendering starts');
+        fetchCityJSONData();
+    }, []);
+	
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="row">
+   <h1>Cost of living in cities</h1>
+   <div className="BarChart">
+   <div className="YAxis">
+   </div>
+    <div className="BarChart-bar" style={{ height: '24.98%'}}>
+      <div className = "BarChart-stack barchart--apartment" style={{height: '72.83%'}}>
+         <p className="barchart--label">Shanghai</p>
+      </div>
+      <div className = "BarChart-stack barchart--utilities" style={{height: '4.4%'}}></div>
+      <div className = "BarChart-stack barchart--food" style={{height: '22.77%'}}></div>
+   </div>
+   <div className="BarChart-bar" style={{ height: '13.11%'}}>
+      <div className = "BarChart-stack barchart--apartment" style={{height: '66.4%'}}>
+         <p className="barchart--label">Mumbai</p>
+      </div>
+      <div className = "BarChart-stack barchart--utilities" style={{height: '5.54%'}}></div>
+      <div className = "BarChart-stack barchart--food" style={{height: '28.06%'}}></div>
+   </div>
+   </div>
+   
+
+  <div className="Legend">
+	<div className="Legend-item">
+	 <div className="Legend-color barchart--apartment"></div>
+	 <p className="Legend-label">Apartment Rent</p>
+	</div>
+	<div className="Legend-item">
+	 <div className="Legend-color barchart--utilities"></div>
+	 <p className="Legend-label">Utilities</p>
+	</div>
+	<div className="Legend-item">
+	 <div className="Legend-color barchart--food"></div>
+	 <p className="Legend-label">Food</p>
+	</div>
+ </div>	
+</div>	
+ 
   );
 }
 
