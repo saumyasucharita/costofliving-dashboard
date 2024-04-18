d459b3e71b136b2fb08b75893c69734d11ec7fd7
saumya <saumya.sucharita@gmail.com>
2024-04-17 19:12:38 -0700

    Calculated cost of living bar heights for first 10 cities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-
 function App() {
 console.log('Starting front-end project');
-const [results, setResults] = useState([]);
+/*const [cost_data, setCostData] = useState([{
+	cityName: 'Shanghai',
+	totalHeight: '24.98%',
+	rentHeight: '72.83%',
+	utilitiesHeight: '4.4%',
+	foodHeight: '22.77%',
+	},
+	{
+	cityName: 'Mumbai',
+	totalHeight: '13.11%',
+	rentHeight: '66.4%',
+	utilitiesHeight: '5.54%',
+	foodHeight: '28.06%',
+	}
+	]);*/
+
+const [cost_data, setCostData] = useState([]);
+
+const yAxisLabels = []
+for (let i = 6000; i >= 500; i -= 500) {
+            yAxisLabels.push(<div className="YAxis-label">{i}</div>);
+      }
 
 function fetchCityJSONData() {
  console.log('In fetch function');	
@@ -15,8 +35,38 @@ function fetchCityJSONData() {
             return res.json();
         })
         .then((data) => {
-           console.log(data);
-           return data;})
+           //console.log(data);
+           const first_10_cities = data.slice(0, 10);
+           console.log(first_10_cities);
+           const bar_height_data = []
+           for(let cityData of first_10_cities)
+           {
+           	const entry = {};
+           	/*console.log('Each city data', cityData);
+           	console.log('For city', cityData['city']);
+        	console.log('Apartment rent is: ', cityData['x48']);
+        	console.log('Utilities: ', cityData['x36']);
+        	console.log('Meal cost for 60 meals: ', cityData['x1']*60);*/
+        	
+        	//Calculate total living expense for the month
+        	let totalSum = parseFloat(cityData['x48']) + parseFloat(cityData['x36']) + parseFloat(cityData['x1']*60);
+        	let barHeight = totalSum/6000 * 100;
+		
+		//Set object properties like total bar-height
+		entry.totalHeight = barHeight;
+		entry.rent = cityData['x48'];
+		entry.cityName = cityData['city'];
+		entry.rentHeight = parseFloat(cityData['x48'])/totalSum * 100;
+		entry.utilitiesHeight = parseFloat(cityData['x36'])/totalSum * 100;
+		entry.foodHeight = parseFloat(cityData['x1']*60)/totalSum * 100;
+		
+		//console.log(entry);
+		bar_height_data.push(entry);
+        	
+           }
+           console.log(bar_height_data);
+           setCostData(bar_height_data);
+           })
         .catch((error) => {
             console.error("Error fetching or parsing data:", error);
             throw error; 
@@ -33,8 +83,9 @@ useEffect(() => {
    <h1>Cost of living in cities</h1>
    <div className="BarChart">
    <div className="YAxis">
+   {yAxisLabels}
    </div>
-    <div className="BarChart-bar" style={{ height: '24.98%'}}>
+    {/*<div className="BarChart-bar" style={{ height: '24.98%'}}>
       <div className = "BarChart-stack barchart--apartment" style={{height: '72.83%'}}>
          <p className="barchart--label">Shanghai</p>
       </div>
@@ -47,7 +98,18 @@ useEffect(() => {
       </div>
       <div className = "BarChart-stack barchart--utilities" style={{height: '5.54%'}}></div>
       <div className = "BarChart-stack barchart--food" style={{height: '28.06%'}}></div>
-   </div>
+   </div>*/}
+   {
+      	cost_data.map(city => (
+      	<div className="BarChart-bar" style={{height: city.totalHeight+ "%"}}>
+      	 <div className = "BarChart-stack barchart--apartment" style={{height: city.rentHeight+ "%"}}>
+         <p className="barchart--label">{city.cityName}</p>
+         </div>
+         <div className = "BarChart-stack barchart--utilities" style={{height: city.utilitiesHeight+ "%"}}></div>
+        <div className = "BarChart-stack barchart--food" style={{height: city.foodHeight+ "%"}}></div>
+       </div>
+   ))
+   }
    </div>
    
 
